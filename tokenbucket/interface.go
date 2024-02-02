427d6c007484ac927f72a0d499f1bccbb95beba0
Xinyue.Wang <Xinyue.Wang@microsoft.com>
2024-02-02 15:13:58 -0800

    rm unused

--- tokenbucket/interface.go
+++
@@ -1,9 +0,0 @@
-package tokenbucket
-
-import "time"
-
-type CacheClient interface {
-	UpdateCache(key string, cacheData map[string]string, expireTime time.Duration) error
-	GetCache(key string) (map[string]string, error)
-	GetMemoryUsage(key string)(int64, error)
-}
